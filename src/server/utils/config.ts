dab30740f4dc168b781c38183e3b1afc12c75c8b
Abhilash Thakur <abhilashthakur.sss@gmail.com>
2020-08-22 17:34:02 +0400

    Update config.ts

--- src/server/utils/config.ts
+++ src/server/utils/config.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-dotenv.config({ path: __dirname + '/../../../.env' });
+// dotenv.config({ path: __dirname + '/../../../.env' });
 
 export default class Config {
   /**
